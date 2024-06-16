ef5810d1a857f0d473cded5799132852f69eac36
antoniotarricone <antonio.tarricone@pagopa.it>
2024-06-16 10:00:31 +0200

    Added retry with backoff on 429 from Azure Key Vault.

--- src/main/java/it/pagopa/swclient/mil/azureservices/util/WebAppExcUtils.java
+++ src/main/java/it/pagopa/swclient/mil/azureservices/util/WebAppExcUtils.java
@@ -33,6 +33,21 @@ public class WebAppExcUtils {
 			return false;
 		}
 	}
+	
+	/**
+	 * 
+	 * @param failure
+	 * @return
+	 */
+	public static boolean isTooManyRequests(Throwable failure) {
+		Log.debug("Failure inspection");
+		if (failure instanceof WebApplicationException webException) {
+			return isTooManyRequests(webException);
+		} else {
+			Log.debugf("Other failure received", failure);
+			return false;
+		}
+	}
 
 	/**
 	 * 
@@ -48,6 +63,24 @@ public class WebAppExcUtils {
 			return true;
 		} else {
 			Log.debugf("HTTP status other than 401 or 403 received: %d", status);
+			return false;
+		}
+	}
+	
+	/**
+	 * 
+	 * @param webException
+	 * @return
+	 */
+	public static boolean isTooManyRequests(WebApplicationException webException) {
+		Log.debug("WebApplicationException inspection");
+		int status = webException.getResponse().getStatus();
+		boolean check = status == 429;
+		if (check) {
+			Log.debug("Too many requests!");
+			return true;
+		} else {
+			Log.debugf("HTTP status other than 429 received: %d", status);
 			return false;
 		}
 	}
